6d2a4b1b115cb9130cf0604f0ccce18ff9288475
S Pearman <monkpearman@icloud.com>
2024-04-10 13:03:54 -0400

    whitespace

--- unicly-loadtime-bind.lisp
+++ unicly-loadtime-bind.lisp
@@ -6,7 +6,7 @@
 (in-package #:unicly)
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
-  
+
   (setf *uuid-namespace-dns*  (make-uuid-from-string "6ba7b810-9dad-11d1-80b4-00c04fd430c8"))
 
   (setf *uuid-namespace-url*  (make-uuid-from-string "6ba7b811-9dad-11d1-80b4-00c04fd430c8"))
@@ -14,7 +14,7 @@
   (setf *uuid-namespace-oid*  (make-uuid-from-string "6ba7b812-9dad-11d1-80b4-00c04fd430c8"))
 
   (setf *uuid-namespace-x500* (make-uuid-from-string "6ba7b814-9dad-11d1-80b4-00c04fd430c8"))
-  
+
   ;; set *uuid-null-uuid* to instance of class `unique-universal-identifier-null'
   (%make-null-uuid-loadtime)
 
